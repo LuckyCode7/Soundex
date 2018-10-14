6ce30be657167f1003e6ede44b141d58a45c60ad
LuckyCode7 <nowak9.private@gmail.com>
2018-10-14 16:44:40 +0200

    Skelton ot the project

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "Soundex.hpp"
+
+int main()
+{
+    std::cout << "Soundex!" << std::endl;
+    return 0;
+}
---
+++ test/SoundexTests.cpp
@@ -0,0 +1,11 @@
+#include <gtest/gtest.h>
+#include "Soundex.hpp"
+
+struct SoundexTests : public ::testing::Test
+{
+};
+
+TEST_F(SoundexTests, assertThatTrueIsTrue)
+{
+    ASSERT_TRUE(true);
+}
